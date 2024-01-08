6a58cf7e831c238b3fce3ac4c0890ef5cc8a532f
theshameem <theshameem.alam@gmail.com>
2024-01-08 18:49:52 +0600

    feat: module update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,50 @@
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  MAT_DIALOG_DEFAULT_OPTIONS,
+  MatDialogModule,
+} from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CategoryListComponent } from './category-list/category-list.component';
+import { CreateCategoryModalComponent } from './create-category-modal/create-category-modal.component';
+import { ClickOutsideDirective } from './directives/click-outside.directive';
 import { NavbarComponent } from './navbar/navbar.component';
 
 const routes: Routes = [
   { path: 'admin/category-list', component: CategoryListComponent },
 ];
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, CategoryListComponent],
-  imports: [BrowserModule, AppRoutingModule, RouterModule.forChild(routes)],
-  providers: [],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    CategoryListComponent,
+    CreateCategoryModalComponent,
+    ClickOutsideDirective,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forChild(routes),
+    MatDialogModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
+  providers: [
+    { provide: MAT_DIALOG_DEFAULT_OPTIONS, useValue: { hasBackdrop: false } },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
