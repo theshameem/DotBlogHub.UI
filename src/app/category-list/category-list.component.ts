32aae8f7d25e12dd3e952dbe1d99f0deb1e57240
theshameem <theshameem.alam@gmail.com>
2024-01-08 18:50:12 +0600

    feat: add category modal open

--- src/app/category-list/category-list.component.ts
+++ src/app/category-list/category-list.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { CreateCategoryModalComponent } from '../create-category-modal/create-category-modal.component';
 
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
-  styleUrls: ['./category-list.component.scss']
+  styleUrls: ['./category-list.component.scss'],
 })
 export class CategoryListComponent {
+  isModalOpen: boolean = false;
+  constructor(public dialog: MatDialog) {}
 
+  ngOnInit(): void {}
+
+  openCreateCategoryModal(): void {
+    this.isModalOpen = true;
+    console.log('openCreateCategoryModal()');
+    const dialogRef = this.dialog.open(CreateCategoryModalComponent, {
+      width: '400px',
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      this.isModalOpen = false;
+    });
+  }
 }
