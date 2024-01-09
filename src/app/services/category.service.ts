2909a56e7e6158ec912424c3cfe71f0fe5aadc07
theshameem <theshameem.alam@gmail.com>
2024-01-09 21:21:23 +0600

    feat: get and post payload model add

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -1,24 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { AddCategoryRequest } from '../../models/add-category-request.model';
+import { Category } from '../../models/category.model';
 
 @Injectable()
 export class CategoryService {
   constructor(private http: HttpClient) {}
 
-  addCategory(model: any): Observable<void> {
-    const payload = {
-      name: model.categoryName,
-      urlHandle: model.urlHandler,
-    };
+  addCategory(payload: AddCategoryRequest): Observable<void> {
     const url = `https://localhost:7045/api/categories`;
 
     return this.http.post<void>(url, payload);
   }
 
-  getCategoryList(): Observable<void> {
+  getCategoryList(): Observable<Category[]> {
     const url = `https://localhost:7045/api/Categories`;
 
-    return this.http.get<void>(url);
+    return this.http.get<Category[]>(url);
   }
 }
