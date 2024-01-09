46528056af5e1024d36e24d958390e8cc1fae647
theshameem <theshameem.alam@gmail.com>
2024-01-09 21:07:06 +0600

    feat: category list integration

--- src/app/category-list/category-list.component.ts
+++ src/app/category-list/category-list.component.ts
@@ -1,17 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { Subject, takeUntil } from 'rxjs';
 import { CreateCategoryModalComponent } from '../create-category-modal/create-category-modal.component';
+import { CategoryService } from '../services/category.service';
 
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
   styleUrls: ['./category-list.component.scss'],
 })
-export class CategoryListComponent {
+export class CategoryListComponent implements OnInit, OnDestroy {
   isModalOpen: boolean = false;
-  constructor(public dialog: MatDialog) {}
+  categoryList: any = [];
+  private _unsubscribeAll: Subject<void> = new Subject<any>();
 
-  ngOnInit(): void {}
+  constructor(
+    public dialog: MatDialog,
+    private categoryService: CategoryService
+  ) {}
+
+  ngOnInit(): void {
+    this.getCategories();
+  }
 
   openCreateCategoryModal(): void {
     this.isModalOpen = true;
@@ -22,5 +32,19 @@ export class CategoryListComponent {
     dialogRef.afterClosed().subscribe((result) => {
       this.isModalOpen = false;
     });
+  }
+
+  getCategories(): void {
+    this.categoryService
+      .getCategoryList()
+      .pipe(takeUntil(this._unsubscribeAll))
+      .subscribe((res) => {
+        this.categoryList = res;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this._unsubscribeAll.complete();
+    this._unsubscribeAll.next();
   }
 }
