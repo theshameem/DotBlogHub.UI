c7fe25a44843225c81476e0edc08979605944683
theshameem <theshameem.alam@gmail.com>
2024-01-12 12:47:36 +0600

    feat: edit and add category functionality in same modal

--- src/app/create-category-modal/create-category-modal.component.ts
+++ src/app/create-category-modal/create-category-modal.component.ts
@@ -1,8 +1,15 @@
-import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
+import {
+  Component,
+  HostListener,
+  Inject,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MatDialogRef } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Subject, takeUntil } from 'rxjs';
 import { AddCategoryRequest } from '../../models/add-category-request.model';
+import { Category } from '../../models/category.model';
 import { CategoryService } from '../services/category.service';
 
 @Component({
@@ -12,22 +19,25 @@ import { CategoryService } from '../services/category.service';
 })
 export class CreateCategoryModalComponent implements OnInit, OnDestroy {
   categoryForm!: FormGroup;
+  modalTitle: string = 'Create category';
   private _unsubscribeAll: Subject<void> = new Subject<any>();
 
   constructor(
     private formBuilder: FormBuilder,
     private categoryService: CategoryService,
-    public dialogRef: MatDialogRef<CreateCategoryModalComponent>
+    public dialogRef: MatDialogRef<CreateCategoryModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: Category
   ) {}
 
   ngOnInit(): void {
+    this.data ? (this.modalTitle = 'Edit Category') : '';
     this.initForm();
   }
 
   initForm(): void {
     this.categoryForm = this.formBuilder.group({
-      categoryName: ['', Validators.required],
-      urlHandler: ['', Validators.required],
+      categoryName: [this.data?.name || '', Validators.required],
+      urlHandler: [this.data?.urlHandle || '', Validators.required],
     });
   }
 
@@ -37,14 +47,28 @@ export class CreateCategoryModalComponent implements OnInit, OnDestroy {
         name: this.categoryForm.value.categoryName,
         urlHandle: this.categoryForm.value.urlHandler,
       };
-
-      this.categoryService
-        .addCategory(payload)
-        .pipe(takeUntil(this._unsubscribeAll))
-        .subscribe((res: any) => {
-          this.dialogRef.close(true);
-        });
+      if (this.data) {
+        //Do update category call
+        payload.id = this.data.id;
+        this.updateCategory(payload);
+      } else {
+        //Do add category api call
+        this.addCategory(payload);
+      }
     }
+  }
+
+  addCategory(payload: AddCategoryRequest): void {
+    this.categoryService
+      .addCategory(payload)
+      .pipe(takeUntil(this._unsubscribeAll))
+      .subscribe((res: any) => {
+        this.dialogRef.close(true);
+      });
+  }
+
+  updateCategory(payload: AddCategoryRequest): void {
+    console.log('payload: ', payload);
   }
 
   @HostListener('window:keyup.esc') onKeyUp() {
