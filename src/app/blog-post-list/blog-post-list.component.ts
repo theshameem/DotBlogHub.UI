aa77496839ba4f588dacfa91eead7ab14ad1aed8
theshameem <theshameem.alam@gmail.com>
2024-01-15 20:45:52 +0600

    feat: blog post list component init

---
+++ src/app/blog-post-list/blog-post-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-post-list',
+  templateUrl: './blog-post-list.component.html',
+  styleUrls: ['./blog-post-list.component.scss'],
+})
+export class BlogPostListComponent {}
