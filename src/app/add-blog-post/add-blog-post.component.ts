4b415c4d74d0ac6e7004afbe09ccaa41ac32792d
theshameem <theshameem.alam@gmail.com>
2024-02-05 09:46:01 +0600

    feat: blog post model create

--- src/app/add-blog-post/add-blog-post.component.ts
+++ src/app/add-blog-post/add-blog-post.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { AddBlogPost } from 'src/models/add-blog-post.model';
 
 @Component({
   selector: 'app-add-blog-post',
   templateUrl: './add-blog-post.component.html',
-  styleUrls: ['./add-blog-post.component.scss']
+  styleUrls: ['./add-blog-post.component.scss'],
 })
 export class AddBlogPostComponent {
+  blogPostModel: AddBlogPost;
 
+  constructor() {
+    this.blogPostModel = {
+      title: '',
+      shortDescription: '',
+      urlHandle: '',
+      content: '',
+      featuredImageUrl: '',
+      author: '',
+      isVisible: true,
+      publishedDate: new Date(),
+    };
+  }
+
+  onFormSubmit(): void {
+    console.log(this.blogPostModel);
+  }
 }
