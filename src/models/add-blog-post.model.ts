720b1c8860532bdc6c9483429745a7c6253a38fb
theshameem <theshameem.alam@gmail.com>
2024-02-05 09:41:08 +0600

    feat: add blog post model

---
+++ src/models/add-blog-post.model.ts
@@ -0,0 +1,10 @@
+export interface AddBlogPost {
+  title: string;
+  urlHandle: string;
+  shortDescription: string;
+  content: string;
+  featuredImageUrl: string;
+  publishedDate: Date;
+  author: string;
+  isVisible: boolean;
+}
