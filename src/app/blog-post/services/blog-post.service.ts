5dea948dcebce74713d5c7ec0da8ac38ae8d0006
theshameem <theshameem.alam@gmail.com>
2024-02-07 06:32:43 +0600

    feat: create blog post api implementation

--- src/app/blog-post/services/blog-post.service.ts
+++ src/app/blog-post/services/blog-post.service.ts
@@ -1,6 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { AddBlogPost } from '../models/add-blog-post.model';
+import { BlogPost } from '../models/blog-post.model';
 
 @Injectable()
 export class BlogPostService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  createBlogPost(payload: AddBlogPost): Observable<BlogPost> {
+    const url = `${environment.apiBaseUrl}/api/blogposts`;
+    return this.http.post<BlogPost>(url, payload);
+  }
 }
