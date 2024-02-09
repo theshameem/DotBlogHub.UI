0f671ca7e377c1775d8f7f4d79beb41c5469e0b1
theshameem <theshameem.alam@gmail.com>
2024-02-09 19:26:01 +0600

    feat: get all blog posts implementation

--- src/app/blog-post/blog-post-list/blog-post-list.component.ts
+++ src/app/blog-post/blog-post-list/blog-post-list.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { BlogPost } from '../models/blog-post.model';
+import { BlogPostService } from '../services/blog-post.service';
 
 @Component({
   selector: 'app-blog-post-list',
   templateUrl: './blog-post-list.component.html',
   styleUrls: ['./blog-post-list.component.scss'],
 })
-export class BlogPostListComponent {}
+export class BlogPostListComponent implements OnInit, OnDestroy {
+  blogPosts$?: Observable<BlogPost[]>;
+  private _unsubsribeAll: Subject<void> = new Subject<any>();
+
+  constructor(private blogPostService: BlogPostService) {}
+
+  ngOnInit(): void {
+    this.blogPosts$ = this.blogPostService.getBlogList();
+  }
+
+  ngOnDestroy(): void {
+    this._unsubsribeAll.complete();
+    this._unsubsribeAll.next();
+  }
+}
