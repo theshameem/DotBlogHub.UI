44a87bd4804cb70c74ed6770e87965c339bf1ff3
theshameem <theshameem.alam@gmail.com>
2024-04-06 19:36:19 +0600

    feat: edit blog post data integration

--- src/app/blog-post/edit-blogpost/edit-blogpost.component.ts
+++ src/app/blog-post/edit-blogpost/edit-blogpost.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { BlogPost } from '../models/blog-post.model';
+import { BlogPostService } from '../services/blog-post.service';
 
 @Component({
   selector: 'app-edit-blogpost',
@@ -10,15 +12,41 @@ import { Subscription } from 'rxjs';
 export class EditBlogpostComponent implements OnInit, OnDestroy {
   id: string | null = null;
   routeSubscription?: Subscription;
+  blogPostModel?: BlogPost;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private blogPostService: BlogPostService
+  ) {}
 
   ngOnInit(): void {
     this.routeSubscription = this.route.paramMap.subscribe({
       next: (params) => {
         this.id = params.get('id');
+
+        // Get BlogPost from API
+        if (this.id) {
+          this.blogPostService.getBlogPostId(this.id).subscribe({
+            next: (response) => {
+              this.blogPostModel = response;
+            },
+          });
+        }
       },
     });
+  }
+
+  onFormSubmit(): void {
+    // if(this.blogPostModel) {
+    //   this.blogPostService.createBlogPost(this.blogPostModel).subscribe({
+    //     next: (response) => {
+    //       this.router.navigateByUrl('/admin/blogpost-list');
+    //     },
+    //     error: (e) => {
+    //       console.error('Something went wrong')
+    //     },
+    //   });
+    // }
   }
 
   ngOnDestroy(): void {
